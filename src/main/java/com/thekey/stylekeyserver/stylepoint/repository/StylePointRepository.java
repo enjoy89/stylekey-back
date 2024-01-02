93be722090248fd65fd286954dd8b4d89e024e77
jeondui <jeondui@gmail.com>
2024-01-02 19:46:12 +0900

    refactor(StylePointRepository): 패키지 구조 수정

--- src/main/java/com/thekey/stylekeyserver/stylepoint/repository/StylePointRepository.java
+++ src/main/java/com/thekey/stylekeyserver/stylepoint/repository/StylePointRepository.java
@@ -1,5 +1,6 @@
-package com.thekey.stylekeyserver.stylepoint.domain;
+package com.thekey.stylekeyserver.stylepoint.repository;
 
+import com.thekey.stylekeyserver.stylepoint.domain.StylePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
