2c6a0033f8c2632acc437220d54246666f8e2236
jeondui <jeondui@gmail.com>
2024-01-02 19:45:48 +0900

    refactor(BrandRepository): 패키지 구조 수정

--- src/main/java/com/thekey/stylekeyserver/brand/repository/BrandRepository.java
+++ src/main/java/com/thekey/stylekeyserver/brand/repository/BrandRepository.java
@@ -1,5 +1,6 @@
-package com.thekey.stylekeyserver.brand.domain;
+package com.thekey.stylekeyserver.brand.repository;
 
+import com.thekey.stylekeyserver.brand.domain.Brand;
 import com.thekey.stylekeyserver.stylepoint.domain.StylePoint;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
